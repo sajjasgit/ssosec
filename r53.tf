a011b8a7a5d4094200f16d7bf0e3b611c878ca38
Sudhakar <sajjasgit@gmail.com>
2022-01-09 15:45:36 +0530

    First commit

--- r53.tf
+++ r53.tf
@@ -3,7 +3,7 @@ data "aws_route53_zone" "primary" {
 }
 
 resource "aws_route53_record" "ssosec-r53" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = data.aws_route53_zone.primary.zone_id
   name    = "${var.env}.${var.hostedzone}"
   type    = "A"
   ttl     = "60"
