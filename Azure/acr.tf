9bb98bb444645b9392a50a2c661d50113d60e37d
sudhakar <sajjasgit@gmail.com>
2022-01-14 16:28:22 +0530

    updated acr name

--- Azure/acr.tf
+++ Azure/acr.tf
@@ -1,5 +1,6 @@
 resource "azurerm_container_registry" "ssosec_acr" {
-  name                = "${var.prefix}-acr"
+  # name                = "${var.prefix}-acr" # error alpha numeric
+  name                = "ssosecacr"
   location            = azurerm_resource_group.ssosec_rg.location
   resource_group_name = azurerm_resource_group.ssosec_rg.name
   retention_policy {
