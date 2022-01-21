a3feae23966c677b954e92afa40a0b7d1e64e64a
Sudhakar <sajjasgit@gmail.com>
2022-01-21 20:45:35 +0530

    updated role

--- Azure/role.tf
+++ Azure/role.tf
@@ -1,38 +1,18 @@
-resource "random_string" "vm_sp_secret" {
-  length  = 32
-  special = true
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "ssosec_acr" {
+  display_name = "ssosec_app"
+  owners       = [data.azuread_client_config.current.object_id]
 }
 
-resource "time_rotating" "this" {
-  rotation_days = 7
-}
-
-resource "azuread_application" "ssosec_vm_app" {
-  # name         = "${var.prefix}-acr-app"
-  display_name = "${var.prefix}-acr-app"
-}
-
-resource "azuread_service_principal" "ssosec_vm_sp" {
-  application_id               = azuread_application.ssosec_vm_app.application_id
+resource "azuread_service_principal" "ssosec_acr_sp" {
+  application_id               = azuread_application.ssosec_acr.application_id
   app_role_assignment_required = false
-}
-
-resource "azuread_service_principal_password" "ssosec_vm_sp" {
-  service_principal_id = azuread_service_principal.ssosec_vm_sp.id
-  rotate_when_changed = {
-    rotation = time_rotating.this.id
-  }
-}
-
-resource "azuread_application_password" "ssosec_vm_sp" {
-  application_object_id = azuread_application.ssosec_vm_app.id
-  rotate_when_changed = {
-    rotation = time_rotating.this.id
-  }
+  owners                       = [data.azuread_client_config.current.object_id]
 }
 
 resource "azurerm_role_assignment" "aks_sp_container_registry" {
   scope                = azurerm_linux_virtual_machine.ssosec_vm.id
   role_definition_name = "AcrPull"
-  principal_id         = azuread_service_principal.ssosec_vm_sp.object_id
+  principal_id         = azuread_service_principal.ssosec_acr_sp.object_id
 }
