3938841f9d27d678c37ca6a3d400eed2f7c4ce16
Sudhakar <sajjasgit@gmail.com>
2022-01-26 17:10:41 +0530

    bug fix

--- Azure/vm.tf
+++ Azure/vm.tf
@@ -3,7 +3,7 @@ resource "azurerm_linux_virtual_machine" "ssosec_vm" {
   location              = azurerm_resource_group.ssosec_rg.location
   resource_group_name   = azurerm_resource_group.ssosec_rg.name
   network_interface_ids = [azurerm_network_interface.ssosec_nic.id]
-  user_data             = base64encode(templatefile("${path.module}/scripts/install_app.sh", { APP_ACR_REPO_URL = azurerm_container_registry.ssosec_acr.login_sever, REGION = var.region }))
+  user_data             = base64encode(templatefile("${path.module}/scripts/install_app.sh", { APP_ACR_REPO_URL = azurerm_container_registry.ssosec_acr.login_server, REGION = var.region }))
   # delete_os_disk_on_termination    = true
   # delete_data_disks_on_termination = true
   size           = "Standard_B2s"
