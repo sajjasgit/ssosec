5c32122b4591a48b28d005622ff3ea5edae08085
SudhakaraRao Sajja <59761354+sajjasgit@users.noreply.github.com>
2022-04-02 12:08:38 +0530

    Update network.tf

--- Azure/network.tf
+++ Azure/network.tf
@@ -30,7 +30,7 @@ resource "azurerm_network_interface" "ssosec_nic" {
   ip_configuration {
     name                          = "${var.prefix}-nic-config"
     subnet_id                     = azurerm_subnet.ssosec_subnet.id
-    private_ip_address_allocation = "dynamic"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.ssosec_public_ip.ip_address
   }
 }
